cba1aa4835fbcd8e84baa39cda12218e38993209
Rishabh Sambare <59372162+rishabhsamb@users.noreply.github.com>
2021-06-06 16:55:56 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,11 +5,11 @@ const port = 3001
 const redis = require('redis')
 const client = redis.createClient()
 const { promisify } = require('util')
-const getAsync = promisify(client.get).bind(client)
+const getAsync = promisify(client.get).bind(client) // asynchronously reads from Redis key:value store, given key as a string.
 
 app.get('/papers', async (req, res) => {
     const papers = await getAsync('papers')
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000")
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000") // FOR DEV PURPOSES ONLY.
     return res.send(papers)
 })
 
