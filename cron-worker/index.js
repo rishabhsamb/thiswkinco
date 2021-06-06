6f431745dc8e28458ad7f3ab137b902cdf10c75a
Rishabh Sambare <59372162+rishabhsamb@users.noreply.github.com>
2021-06-06 16:57:35 -0400

    Update index.js

--- cron-worker/index.js
+++ cron-worker/index.js
@@ -2,5 +2,5 @@ const CronJob = require('cron').CronJob;
 
 const fetchArxiv = require('./fetch-arxiv.js')
 
-var job = new CronJob('30 3 * * * *', fetchArxiv, null, true, 'America/New_York');
+var job = new CronJob('0 0 * * 0', fetchArxiv, null, true, 'America/New_York'); // call fetchArxiv at 00:00 every Sunday.
 job.start();
